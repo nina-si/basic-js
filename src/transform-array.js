bd5681d4c7c5a715a25f103931f37c56588b6aa3
Nina Sitaeva <sitaeva.nn@gmail.com>
2020-11-01 23:47:01 +0300

    transform arr

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (Array.isArray(arr)) {
+    let newArray = [];
+    for (i = 0; i < arr.length; i++) {
+
+      if (arr[i] === '--discard-next') {
+          i++;
+      } else if (arr[i] === '--discard-prev') {
+          if (newArray.length !== 0 && arr[i - 2] !== '--discard-next') {
+              newArray.pop();
+          }
+      } else if (arr[i] === '--double-next') {
+          if (i + 1 < arr.length) {
+            newArray.push(arr[i + 1]);
+          }
+      } else if (arr[i] === '--double-prev') {
+          if (i !== 0 && arr[i - 2] !== '--discard-next') {
+              newArray.push(arr[i - 1]);
+          }
+      } else {
+          newArray.push(arr[i]);
+      }
+    }
+    return newArray;
+  } else {
+    throw new Error();
+  }
 };
